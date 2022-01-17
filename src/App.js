84fe76e7b9eeb78f3d0504a9fcca8aad8d571a3d
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2022-01-17 20:41:09 +0530

    movie

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
 import "./styles.css";
 
 const FEATURE_API =
-  "https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher";
-
-const IMG_API =
-  "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1*";
 
 const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher";
+  "https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query=";
 function App() {
-  const movies = ["1", "2", "3"];
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(FEATURE_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm("");
+    }
+  };
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
-    <div>
-      {movies.map((movie) => (
-        <Movie />
-      ))}
-    </div>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search.."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
   );
 }
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,37 @@
 import React from "react";
+import "../styles.css";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const setVoterClass = (vote) => {
+  console.log(vote);
+  if (vote.vote_average >= 8) {
+    return "green";
+  } else if (vote.vote_average >= 6) {
+    return "orange";
+  } else {
+    return "red";
+  }
+};
 
-const Movie = () => <div className="movie">This is a movie component</div>;
+const Movie = ({ title, poster_path, overview, vote_average }) => (
+  <div className="movie">
+    <img
+      src={
+        poster_path
+          ? IMG_API + poster_path
+          : "https://media.istockphoto.com/photos/pop-corn-and-on-red-armchair-cinema-picture-id1271522601?b=1&k=20&m=1271522601&s=170667a&w=0&h=azZRRchShbrwRgq58omc1HOYABnfDDOzXJatuaZrueQ="
+      }
+      alt={title}
+    />
+    <div className="movie-info">
+      <h3>{title}</h3>
+      <span className={`tag ${setVoterClass({ vote_average })}`}>
+        {vote_average}
+      </span>
+    </div>
+    <div className="movie-over">
+      <h2>Overview:</h2>
+      <p>{overview}</p>
+    </div>
+  </div>
+);
 export default Movie;
